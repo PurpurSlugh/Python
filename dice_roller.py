a04d1ec7ee951c86635c8981f99ecbacd5a70d25
PurpurSlugh <watb.fulminozzo@gmail.com>
2026-02-23 21:44:27 +0100

    Add files via upload

---
+++ dice_roller.py
@@ -0,0 +1,19 @@
+import random
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+print("Insert the number of rolls for the dice: ")
+dice = int(input())
+tails = 0
+heads = 0
+
+for i in range(dice):
+    x = random.randint(1, 2)
+    if (x == 1):
+        print("Heads")
+        heads = heads +1
+    else:
+        print("Tails")
+        tails = tails +1
+
+print("Heads = ",heads / dice * 100, "%")
+print("Tails = ",tails / dice * 100, "%")
